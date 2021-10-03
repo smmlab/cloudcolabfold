5fd281376b3517fb60e20cbe6ae94013ce279425
zach-hensel <zhensel@gmail.com>
2021-10-03 19:30:08 +0100

    initial changes

---
+++ startup_script.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Startup script for cloud instance to process queue
+
+isQueueEmpty()
+{
+    queueEmpty = 0
+    # code to check if queue is empty
+
+    # trigger this w/ if loop
+    queueEmpty = 1
+    echo "Queue is empty"
+}
+
+queueEmpty = 0
+
+# Check if there is anything in the queue
+
+if []
+
+# if here 
+
+# Loop until queue is empty
+
+while []
+do
+    # blah
+    # blah
+done
+
+## Update sequence database
+
+## Get current queue
+
+# Update sequence database to 
